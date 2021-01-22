5cf7b208d9428c7ddc506ba4a4667f31e9a4d408
sujin0529 <201802150@o.cnu.ac.kr>
2021-01-22 14:04:28 +0900

    BinarySearch_treeCutting

---
+++ BJ_2805/src/treeCutting/Main.java
@@ -0,0 +1,57 @@
+package treeCutting;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		try {
+			String[] s = br.readLine().split(" ");
+			int count = Integer.parseInt(s[0]); // 나무 개수
+			int target = Integer.parseInt(s[1]); // 목표 나무 길이
+			int[] tree = new int[count];
+			s = br.readLine().split(" ");
+			for(int i=0; i<count; i++) {
+				tree[i] = Integer.parseInt(s[i]);
+			}
+			Arrays.parallelSort(tree);
+			long result = treeLength(tree, 0, tree[count-1], target);
+			bw.write(result+"\n");
+			br.close();
+			bw.close();
+		}catch(Exception e) {
+			
+		}
+	}
+	
+	static long treeLength(int[] tree, long start, long end, int target) {
+		long treeLength = 0;
+		long maxLength = 0;
+		while(start <= end) {
+			treeLength = (start+end)/(long)2;
+			long cutLength = 0;
+			for(int i=0; i<tree.length; i++) {
+				long tmp = tree[i] - treeLength;
+				if(tmp > 0) {
+					cutLength += tmp; 
+				}
+			}
+			if(cutLength >= target) {
+				start = treeLength + 1;
+				maxLength = maxLength > treeLength ? maxLength : treeLength;
+			}
+			else {
+				end = treeLength - 1;
+			}
+		}
+		return maxLength;
+	}
+}
